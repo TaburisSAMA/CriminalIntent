b6444d179ef903d5042853cc7caea930bd93825e
lyn <taburissama@gmail.com>
2015-07-07 18:45:26 +0800

    Initial commit

---
+++ app/src/main/java/com/example/lyn/criminalintent/Crime.java
@@ -0,0 +1,27 @@
+package com.example.lyn.criminalintent;
+
+import java.util.UUID;
+
+/**
+ * Created by lyn on 15-7-7.
+ */
+public class Crime {
+    private UUID mId;
+    private String mTitle;
+
+    public Crime() {
+        mId = UUID.randomUUID();
+    }
+
+    public UUID getId() {
+        return mId;
+    }
+
+    public String getTitle(){
+        return mTitle;
+    }
+
+    public void setTitle(String title) {
+        mTitle = title;
+    }
+}
